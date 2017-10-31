69b4e1c1417b435ea118cd894e4ce5c87a008410
Matthew Tribby <mrt28@duke.edu>
2017-10-31 17:13:16 -0400

    Initial Project Creation with Firebase Integration and working auth

---
+++ app/src/main/java/com/android/databaes/pantrypals/auth/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.android.databaes.pantrypals.auth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.databaes.pantrypals.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
