ca30742057ba23b1fdc193deeefce868767050f0
Aditya Srinivasan <asrinivasan@cinchapi.com>
2017-11-05 16:36:55 -0500

    Add search bar to discover fragment

--- app/src/main/java/pantrypals/discover/DiscoverFragment.java
+++ app/src/main/java/pantrypals/discover/DiscoverFragment.java
@@ -1,12 +1,16 @@
 package pantrypals.discover;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.databaes.pantrypals.R;
 
@@ -30,7 +34,40 @@ public class DiscoverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_discover, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        // Implementing ActionBar Search inside a fragment
+        MenuItem item = menu.add("Search");
+        item.setIcon(R.drawable.ic_discover); // sets icon
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SearchView sv = new SearchView(getActivity());
+
+        // modifying the text inside edittext component
+        int id = sv.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = sv.findViewById(id);
+        textView.setHint("Search for people, posts, or communities...");
+        textView.setHintTextColor(getResources().getColor(R.color.common_google_signin_btn_text_dark));
+        textView.setTextColor(getResources().getColor(R.color.common_google_signin_btn_text_light));
+
+        // implementing the listener
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // TODO: search on query
+                return true;
+            }
+        });
+        item.setActionView(sv);
     }
 
 
