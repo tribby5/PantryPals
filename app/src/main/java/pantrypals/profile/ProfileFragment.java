7bee207401b5cf3d88e1e086528d2c35cf5913e7
Hunter Lee <narendly@gmail.com>
2017-11-06 21:19:52 -0500

    Add ListView and Adapter to HomeFragment

--- app/src/main/java/pantrypals/profile/ProfileFragment.java
+++ app/src/main/java/pantrypals/profile/ProfileFragment.java
@@ -30,7 +30,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the recipe_layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
