d13acb3b8d22f505de487dbe1a7746b6d2734927
Hunter Lee <narendly@gmail.com>
2017-11-09 03:51:08 -0500

    Fix Post data model for Firebase data retrieval

--- app/src/main/java/pantrypals/home/HomeFragment.java
+++ app/src/main/java/pantrypals/home/HomeFragment.java
@@ -3,54 +3,68 @@ package pantrypals.home;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.databaes.pantrypals.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import pantrypals.activities.HomeActivity;
+import pantrypals.models.Post;
+
 public class HomeFragment extends Fragment {
-    private FirebaseDatabase database;
+    private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference ref;
+    private FirebaseAuth mAuth;
+    private String userId;
 
     private ListView feedListView;
-    private ArrayList<String> feedList = new ArrayList<>();
+    private ArrayList<Post> feedList = new ArrayList<>();
+    private ArrayList<String> tempList = new ArrayList<>();
     private ArrayAdapter<String> feedListViewAdapter;
+    private String oldestPostId;
 
     public HomeFragment() {
         // Required empty public constructor
-        database = FirebaseDatabase.getInstance();
-        ref = database.getReference("recipe");
-        ref.addChildEventListener(new ChildEventListener() {
+
+        // TODO: Use the following current user information for more intelligent feed later on
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userId = user.getUid();
+
+        // Retrieve data from Firebase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        ref = mFirebaseDatabase.getReference("/posts");
+        ref.limitToFirst(10).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                feedList.add(dataSnapshot.getValue(String.class));
-            }
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    oldestPostId = snapshot.getKey();
 
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
+                    dataSnapshot.getChildrenCount();
+                    Post post = snapshot.getValue(Post.class);
+                    String postId = snapshot.getKey();
+                    feedList.add(post);
+                    tempList.add(postId);
+                    Log.e("Retrieved postId: ", postId);
+                }
             }
 
             @Override
@@ -75,13 +89,11 @@ public class HomeFragment extends Fragment {
         // Inflate the recipe_layout first
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        String[] feedItems = {"1", "2", "3"};
-
         feedListView = (ListView) view.findViewById(R.id.feedListView);
         feedListViewAdapter = new ArrayAdapter<String>(
                 getActivity(),
                 android.R.layout.simple_list_item_1,
-                feedItems
+                tempList
         );
         feedListView.setAdapter(feedListViewAdapter);
 
@@ -100,5 +112,7 @@ public class HomeFragment extends Fragment {
     }
 
 
-
+    private void toastMessage(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
 }
