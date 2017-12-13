ee9db2f857419008559ec26c7808580dce28de32
Hunter Lee <narendly@gmail.com>
2017-12-13 17:31:24 -0500

    Add null check to Add items to pantry

--- app/src/main/java/pantrypals/pantry/PersonalPantryFragment.java
+++ app/src/main/java/pantrypals/pantry/PersonalPantryFragment.java
@@ -228,7 +228,7 @@ public class PersonalPantryFragment extends Fragment {
                 EditText itemAmount = (EditText) dialogView.findViewById(R.id.add_item_amount);
                 EditText itemDate = (EditText) dialogView.findViewById(R.id.add_item_date);
                 add_item_name = itemName.getText().toString();
-                add_item_amount = Double.parseDouble(itemAmount.getText().toString());
+                add_item_amount = (itemAmount.getText() != null) ? Double.parseDouble(itemAmount.getText().toString()) : 0.0;
                 add_item_date = itemDate.getText().toString();
 
 
